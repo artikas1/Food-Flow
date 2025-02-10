9bf29ac5152789bd3caea71586f8211e459ebc19
ugnius.spokavicius <uspokavicius@gmail.com>
2025-02-10 12:40:12 +0200

    migrated to tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
+// @ts-ignore
 import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="text-center">
+        <header className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
+          <img src={logo} className="h-40 pointer-events-none animate-spin" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+              className="text-blue-400"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
   );
 }
 
