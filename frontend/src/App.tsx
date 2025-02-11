258dc97a6982d0e19ad24a1a9053a4ad5205d77e
Artiom <artas.ple@gmail.com>
2025-02-11 11:14:38 +0200

    Food-card from list component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header.tsx';
 import Drawer from './components/Drawer.tsx';
 import Background from './components/Background.tsx';
+import FoodCardListings from './components/FoodCardListings.tsx';
 
 function App() {
     const [drawerOpen, setDrawerOpen] = useState(false);
@@ -13,6 +14,7 @@ function App() {
             <Background />
             <Header onMenuIconClick={() => setDrawerOpen(true)} />
             <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)} />
+            <FoodCardListings />
         </>
     );
 }
---
+++ frontend/src/components/FoodCardListings.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { IoLocationOutline } from "react-icons/io5";
+import breadImage from 'C:/Users/artas/Desktop/Java_bootcamp/foodflow/frontend/src/assets/bread1.jpg';
+import Avatar from "@mui/material/Avatar";
+
+export default function MediaControlCard() {
+    const theme = useTheme();
+
+    return (
+        <Card
+            sx={{
+                display: 'flex',
+                maxWidth: '50%',
+                margin: 'auto',
+                marginTop: '1rem',
+                borderRadius: 4,
+                '&:hover': {
+                    cursor: 'pointer',
+                    backgroundColor: theme.palette.grey[200],
+                },
+            }}>
+            <CardMedia
+                component="img"
+                sx={{ width: 151, height: 151, objectFit: 'cover' }}
+                image= {breadImage}
+                alt="Food picture"
+            />
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto', pb:1 }}>
+                    <Typography component="div" variant="h6" sx={{ fontWeight: 600 }}>
+                        Full grain bread
+                    </Typography>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
+                        <IoLocationOutline size={16} />
+                        <Typography
+                            variant="subtitle1"
+                            component="div"
+                            sx={{
+                                color: 'text.secondary',
+                                fontSize: '1rem'
+                            }}
+                        >
+                            Vilnius
+                        </Typography>
+                    </Box>
+
+                </CardContent>
+                <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1 }}>
+                    <Avatar
+                        alt="Remy Sharp"
+                        src="/static/images/avatar/2.jpg"
+                        sx={{ width: 38, height: 38, marginLeft: 'auto' }}
+                    />
+                    <Typography
+                        variant="subtitle1"
+                        component="div"
+                        sx={{
+                            color: 'text.secondary',
+                            fontSize: '0.9rem',
+                            marginLeft: 'auto'
+                        }}
+                    >
+                        4.2/5 (14 reviews)
+                    </Typography>
+                </Box>
+            </Box>
+        </Card>
+    );
+}
