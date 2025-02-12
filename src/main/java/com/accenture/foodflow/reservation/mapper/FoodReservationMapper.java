95686c3e6d7127776c5bccc3e778d76dbf2b9acc
ugnius.spokavicius <uspokavicius@gmail.com>
2025-02-12 21:28:23 +0200

    changed dto

--- src/main/java/com/accenture/foodflow/reservation/mapper/FoodReservationMapper.java
+++ src/main/java/com/accenture/foodflow/reservation/mapper/FoodReservationMapper.java
@@ -1,15 +1,20 @@
 package com.accenture.foodflow.reservation.mapper;
 
 import com.accenture.foodflow.food.entity.Food;
+import com.accenture.foodflow.food.mapper.FoodMapper;
 import com.accenture.foodflow.reservation.dto.FoodReservationResponseDto;
 import com.accenture.foodflow.reservation.entity.FoodReservation;
 import com.accenture.foodflow.user.entity.User;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Component
+@AllArgsConstructor
 public class FoodReservationMapper {
+
+    private final FoodMapper foodMapper;
 
     public FoodReservation toEntity(Food food, User user) {
         return FoodReservation.builder()
@@ -24,7 +29,7 @@ public class FoodReservationMapper {
     public FoodReservationResponseDto toFoodResponseDto(FoodReservation foodReservation) {
         return FoodReservationResponseDto.builder()
                 .id(foodReservation.getId())
-                .foodId(foodReservation.getFood().getId())
+                .food(foodMapper.toResponseDto(foodReservation.getFood()))
                 .userId(foodReservation.getUser().getId())
                 .reservationDate(foodReservation.getReservationDate())
                 .expirationDate(foodReservation.getExpirationDate())
